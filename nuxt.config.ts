af074ce502c689133842f4df5ef77c430d851d4e
mirnumbing <mirnumbing@gmail.com>
2025-10-19 21:47:54 +0200

    create data files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,18 @@
 // nuxt.config.ts
 import { defineNuxtConfig } from 'nuxt/config'
 
+declare const process: any
+
 export default defineNuxtConfig({
-  modules: [
-    '@pinia/nuxt',
-    'vuetify-nuxt-module'
-  ],
-  css: [
-    'vuetify/styles',
-    '@mdi/font/css/materialdesignicons.css'
-  ],
-  typescript: {
-    strict: true
+  modules: ['@pinia/nuxt', 'vuetify-nuxt-module'],
+  css: ['vuetify/styles', '@mdi/font/css/materialdesignicons.css'],
+  typescript: { strict: true },
+
+  runtimeConfig: {
+    // server only
+    mongoUri: process.env.MONGO_URI || '',
+    jwtSecret: process.env.JWT_SECRET || '',
+    // public is exposed to client if needed
+    public: {}
   }
 })
